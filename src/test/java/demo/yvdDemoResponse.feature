ebe284d976b03c10e98b081b0946b3eac5d18d78
Somayajula <PKS059@crb.apmoller.net>
2018-11-02 10:39:46 +0100

    Updated files

--- src/test/java/demo/yvdDemoResponse.feature
+++ src/test/java/demo/yvdDemoResponse.feature
@@ -1,28 +1,32 @@
-Feature: YVD service response validation using Karate
+Feature: YVD FC service response validation using Karate
 
 Background:
 * url baseUrl
 
-Scenario: soap 1.1
+Scenario Outline: Validate the YVD response for FC scenario
 
 Given request read('/resources/YVDRequest40DRY1Equipment.xml')
- 	When soap action baseUrl
-    Then status 200
-  	And print 'response: ', response
+ 	  When soap action baseUrl
+      Then status 200
+  	  And print 'response: ', response
   	* def resp = response/Envelope/Body/ValidateShipmentOnYieldResponse/Shipment/ShipmentId
-  	And print 'resp:',resp
-  	* match resp == 'BALCC0303'
+  	  And print 'resp:',resp
+  	* match resp ==  '<ShipmentId>' 	
+  	* def respValidation = response/Envelope/Body/ValidateShipmentOnYieldResponse/ValidationDetails/ValidationPerEquipment/EquipmentTypeValidationOutcomeDescription
+  	* match respValidation == '<ValidationOutcome>'	
   	
-  # use jdbc to validate
-* def config = { username: 'SDA241', password: 'Welcome@1', url: 'jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=utmtest-scan.crb.apmoller.net)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=UTMTEST)(INSTANCE_NAME=UTMTEST1)))', driverClassName: 'oracle.jdbc.xa.client.OracleXADataSource' }
-* def DbUtils = Java.type('resources.DbUtils')
-* def db = new DbUtils(config)
-* print db
-* def SHIPMENTS = db.readValue("SELECT SHIPMENT_ID FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID ='"+resp+"' ")
-* match SHIPMENTS == resp
-* def SHIPMENTS = db.readRow("SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID ='"+resp+"' ")
-* match SHIPMENTS.SHIPMENT_ID == resp
-* def SHIPMENTS = db.readRows("SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION")
-* match contains { SHIPMENTS: '#(resp)' }
+	  # use jdbc to validate
+	* def config = { username: 'SDA241', password: 'Welcome@1', url: 'jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=utmtest-scan.crb.apmoller.net)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=UTMTEST)(INSTANCE_NAME=UTMTEST1)))', driverClassName: 'oracle.jdbc.xa.client.OracleXADataSource' }
+	* def DbUtils = Java.type('resources.DbUtils')
+	* def db = new DbUtils(config)
+	* print db
+	* def SHIPMENTS1 = db.readValue("SELECT SHIPMENT_ID FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID ='"+resp+"' ")
+	* match SHIPMENTS1 == resp
+	* def SHIPMENTS2 = db.readRow("SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID ='"+resp+"' ")
+	* match SHIPMENTS2.SHIPMENT_ID == resp
+	* def SHIPMENTS3 = db.readRows("SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION")
+	* match SHIPMENTS3.SHIPMENT_ID { SHIPMENT_ID: '#(resp)'}
 
-	
+	Examples:
+	| ShipmentId | ValidationOutcome |
+	| BALCC0303 | Fully Committed |
--- target/cucumberreports/html/report.js
+++ target/cucumberreports/html/report.js
@@ -1,43 +1,18 @@
 $(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("demo/yvdDemoResponse.feature");
 formatter.feature({
   "line": 1,
-  "name": "YVD service response validation using Karate",
+  "name": "YVD FC service response validation using Karate",
   "description": "",
-  "id": "yvd-service-response-validation-using-karate",
+  "id": "yvd-fc-service-response-validation-using-karate",
   "keyword": "Feature"
 });
-formatter.background({
-  "line": 3,
-  "name": "",
-  "description": "",
-  "type": "background",
-  "keyword": "Background"
-});
-formatter.step({
-  "line": 4,
-  "name": "url baseUrl",
-  "keyword": "* "
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "baseUrl",
-      "offset": 4
-    }
-  ],
-  "location": "StepDefs.url(String)"
-});
-formatter.result({
-  "duration": 736608301,
-  "status": "passed"
-});
-formatter.scenario({
+formatter.scenarioOutline({
   "line": 6,
-  "name": "soap 1.1",
+  "name": "Validate the YVD response for FC scenario",
   "description": "",
-  "id": "yvd-service-response-validation-using-karate;soap-1.1",
-  "type": "scenario",
-  "keyword": "Scenario"
+  "id": "yvd-fc-service-response-validation-using-karate;validate-the-yvd-response-for-fc-scenario",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
 });
 formatter.step({
   "line": 8,
@@ -71,63 +46,238 @@ formatter.step({
 });
 formatter.step({
   "line": 14,
-  "name": "match resp \u003d\u003d \u0027BALCC0303\u0027",
+  "name": "match resp \u003d\u003d  \u0027\u003cShipmentId\u003e\u0027",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 15,
+  "name": "def respValidation \u003d response/Envelope/Body/ValidateShipmentOnYieldResponse/ValidationDetails/ValidationPerEquipment/EquipmentTypeValidationOutcomeDescription",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 16,
+  "name": "match respValidation \u003d\u003d \u0027\u003cValidationOutcome\u003e\u0027",
   "keyword": "* "
 });
 formatter.step({
   "comments": [
     {
-      "line": 16,
+      "line": 18,
       "value": "# use jdbc to validate"
     }
   ],
-  "line": 17,
+  "line": 19,
   "name": "def config \u003d { username: \u0027SDA241\u0027, password: \u0027Welcome@1\u0027, url: \u0027jdbc:oracle:thin:@(DESCRIPTION\u003d(ADDRESS_LIST\u003d(ADDRESS\u003d(PROTOCOL\u003dTCP)(HOST\u003dutmtest-scan.crb.apmoller.net)(PORT\u003d1521)))(CONNECT_DATA\u003d(SERVICE_NAME\u003dUTMTEST)(INSTANCE_NAME\u003dUTMTEST1)))\u0027, driverClassName: \u0027oracle.jdbc.xa.client.OracleXADataSource\u0027 }",
   "keyword": "* "
 });
 formatter.step({
-  "line": 18,
+  "line": 20,
   "name": "def DbUtils \u003d Java.type(\u0027resources.DbUtils\u0027)",
   "keyword": "* "
 });
 formatter.step({
-  "line": 19,
+  "line": 21,
   "name": "def db \u003d new DbUtils(config)",
   "keyword": "* "
 });
 formatter.step({
-  "line": 20,
+  "line": 22,
   "name": "print db",
   "keyword": "* "
 });
 formatter.step({
-  "line": 21,
-  "name": "def SHIPMENTS \u003d db.readValue(\"SELECT SHIPMENT_ID FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID \u003d\u0027\"+resp+\"\u0027 \")",
-  "keyword": "* "
-});
-formatter.step({
-  "line": 22,
-  "name": "match SHIPMENTS \u003d\u003d resp",
-  "keyword": "* "
-});
-formatter.step({
   "line": 23,
-  "name": "def SHIPMENTS \u003d db.readRow(\"SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID \u003d\u0027\"+resp+\"\u0027 \")",
+  "name": "def SHIPMENTS1 \u003d db.readValue(\"SELECT SHIPMENT_ID FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID \u003d\u0027\"+resp+\"\u0027 \")",
   "keyword": "* "
 });
 formatter.step({
   "line": 24,
-  "name": "match SHIPMENTS.SHIPMENT_ID \u003d\u003d resp",
+  "name": "match SHIPMENTS1 \u003d\u003d resp",
   "keyword": "* "
 });
 formatter.step({
   "line": 25,
-  "name": "def SHIPMENTS \u003d db.readRows(\"SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION\")",
+  "name": "def SHIPMENTS2 \u003d db.readRow(\"SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID \u003d\u0027\"+resp+\"\u0027 \")",
   "keyword": "* "
 });
 formatter.step({
   "line": 26,
-  "name": "match contains { SHIPMENTS: \u0027#(resp)\u0027 }",
+  "name": "match SHIPMENTS2.SHIPMENT_ID \u003d\u003d resp",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 27,
+  "name": "def SHIPMENTS3 \u003d db.readRows(\"SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION\")",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 28,
+  "name": "match SHIPMENTS3.SHIPMENT_ID { SHIPMENT_ID: \u0027#(resp)\u0027}",
+  "keyword": "* "
+});
+formatter.examples({
+  "line": 30,
+  "name": "",
+  "description": "",
+  "id": "yvd-fc-service-response-validation-using-karate;validate-the-yvd-response-for-fc-scenario;",
+  "rows": [
+    {
+      "cells": [
+        "ShipmentId",
+        "ValidationOutcome"
+      ],
+      "line": 31,
+      "id": "yvd-fc-service-response-validation-using-karate;validate-the-yvd-response-for-fc-scenario;;1"
+    },
+    {
+      "cells": [
+        "BALCC0303",
+        "Fully Committed"
+      ],
+      "line": 32,
+      "id": "yvd-fc-service-response-validation-using-karate;validate-the-yvd-response-for-fc-scenario;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "url baseUrl",
+  "keyword": "* "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "baseUrl",
+      "offset": 4
+    }
+  ],
+  "location": "StepDefs.url(String)"
+});
+formatter.result({
+  "duration": 802157383,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 32,
+  "name": "Validate the YVD response for FC scenario",
+  "description": "",
+  "id": "yvd-fc-service-response-validation-using-karate;validate-the-yvd-response-for-fc-scenario;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 8,
+  "name": "request read(\u0027/resources/YVDRequest40DRY1Equipment.xml\u0027)",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 9,
+  "name": "soap action baseUrl",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "status 200",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "print \u0027response: \u0027, response",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "def resp \u003d response/Envelope/Body/ValidateShipmentOnYieldResponse/Shipment/ShipmentId",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 13,
+  "name": "print \u0027resp:\u0027,resp",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "match resp \u003d\u003d  \u0027BALCC0303\u0027",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "* "
+});
+formatter.step({
+  "line": 15,
+  "name": "def respValidation \u003d response/Envelope/Body/ValidateShipmentOnYieldResponse/ValidationDetails/ValidationPerEquipment/EquipmentTypeValidationOutcomeDescription",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 16,
+  "name": "match respValidation \u003d\u003d \u0027Fully Committed\u0027",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "* "
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 18,
+      "value": "# use jdbc to validate"
+    }
+  ],
+  "line": 19,
+  "name": "def config \u003d { username: \u0027SDA241\u0027, password: \u0027Welcome@1\u0027, url: \u0027jdbc:oracle:thin:@(DESCRIPTION\u003d(ADDRESS_LIST\u003d(ADDRESS\u003d(PROTOCOL\u003dTCP)(HOST\u003dutmtest-scan.crb.apmoller.net)(PORT\u003d1521)))(CONNECT_DATA\u003d(SERVICE_NAME\u003dUTMTEST)(INSTANCE_NAME\u003dUTMTEST1)))\u0027, driverClassName: \u0027oracle.jdbc.xa.client.OracleXADataSource\u0027 }",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 20,
+  "name": "def DbUtils \u003d Java.type(\u0027resources.DbUtils\u0027)",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 21,
+  "name": "def db \u003d new DbUtils(config)",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 22,
+  "name": "print db",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 23,
+  "name": "def SHIPMENTS1 \u003d db.readValue(\"SELECT SHIPMENT_ID FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID \u003d\u0027\"+resp+\"\u0027 \")",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 24,
+  "name": "match SHIPMENTS1 \u003d\u003d resp",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 25,
+  "name": "def SHIPMENTS2 \u003d db.readRow(\"SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID \u003d\u0027\"+resp+\"\u0027 \")",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 26,
+  "name": "match SHIPMENTS2.SHIPMENT_ID \u003d\u003d resp",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 27,
+  "name": "def SHIPMENTS3 \u003d db.readRows(\"SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION\")",
+  "keyword": "* "
+});
+formatter.step({
+  "line": 28,
+  "name": "match SHIPMENTS3.SHIPMENT_ID { SHIPMENT_ID: \u0027#(resp)\u0027}",
   "keyword": "* "
 });
 formatter.match({
@@ -140,7 +290,7 @@ formatter.match({
   "location": "StepDefs.request(String)"
 });
 formatter.result({
-  "duration": 103772088,
+  "duration": 41873300,
   "status": "passed"
 });
 formatter.match({
@@ -153,7 +303,7 @@ formatter.match({
   "location": "StepDefs.soapAction(String)"
 });
 formatter.result({
-  "duration": 7566680630,
+  "duration": 14407343466,
   "status": "passed"
 });
 formatter.match({
@@ -166,7 +316,7 @@ formatter.match({
   "location": "StepDefs.status(int)"
 });
 formatter.result({
-  "duration": 910811,
+  "duration": 776110,
   "status": "passed"
 });
 formatter.match({
@@ -179,7 +329,7 @@ formatter.match({
   "location": "StepDefs.print(String)"
 });
 formatter.result({
-  "duration": 13864205,
+  "duration": 10181824,
   "status": "passed"
 });
 formatter.match({
@@ -196,7 +346,7 @@ formatter.match({
   "location": "StepDefs.def(String,String)"
 });
 formatter.result({
-  "duration": 26353909,
+  "duration": 12205141,
   "status": "passed"
 });
 formatter.match({
@@ -209,7 +359,7 @@ formatter.match({
   "location": "StepDefs.print(String)"
 });
 formatter.result({
-  "duration": 11240733,
+  "duration": 5812895,
   "status": "passed"
 });
 formatter.match({
@@ -221,14 +371,50 @@ formatter.match({
     },
     {},
     {
-      "val": "\u0027BALCC0303\u0027",
+      "val": " \u0027BALCC0303\u0027",
       "offset": 14
     }
   ],
   "location": "StepDefs.matchEquals(String,String,String,String)"
 });
 formatter.result({
-  "duration": 10303476,
+  "duration": 7764280,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "respValidation",
+      "offset": 4
+    },
+    {
+      "val": "response/Envelope/Body/ValidateShipmentOnYieldResponse/ValidationDetails/ValidationPerEquipment/EquipmentTypeValidationOutcomeDescription",
+      "offset": 21
+    }
+  ],
+  "location": "StepDefs.def(String,String)"
+});
+formatter.result({
+  "duration": 1928111,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {},
+    {
+      "val": "respValidation",
+      "offset": 6
+    },
+    {},
+    {
+      "val": "\u0027Fully Committed\u0027",
+      "offset": 24
+    }
+  ],
+  "location": "StepDefs.matchEquals(String,String,String,String)"
+});
+formatter.result({
+  "duration": 6713833,
   "status": "passed"
 });
 formatter.match({
@@ -245,7 +431,7 @@ formatter.match({
   "location": "StepDefs.def(String,String)"
 });
 formatter.result({
-  "duration": 61161818,
+  "duration": 47163617,
   "status": "passed"
 });
 formatter.match({
@@ -262,7 +448,7 @@ formatter.match({
   "location": "StepDefs.def(String,String)"
 });
 formatter.result({
-  "duration": 11773538,
+  "duration": 10123994,
   "status": "passed"
 });
 formatter.match({
@@ -279,7 +465,7 @@ formatter.match({
   "location": "StepDefs.def(String,String)"
 });
 formatter.result({
-  "duration": 190416145,
+  "duration": 121893795,
   "status": "passed"
 });
 formatter.match({
@@ -292,96 +478,96 @@ formatter.match({
   "location": "StepDefs.print(String)"
 });
 formatter.result({
-  "duration": 13831059,
+  "duration": 6988169,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {
-      "val": "SHIPMENTS",
+      "val": "SHIPMENTS1",
       "offset": 4
     },
     {
       "val": "db.readValue(\"SELECT SHIPMENT_ID FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID \u003d\u0027\"+resp+\"\u0027 \")",
-      "offset": 16
+      "offset": 17
     }
   ],
   "location": "StepDefs.def(String,String)"
 });
 formatter.result({
-  "duration": 456130886,
+  "duration": 900243870,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {},
     {
-      "val": "SHIPMENTS",
+      "val": "SHIPMENTS1",
       "offset": 6
     },
     {},
     {
       "val": "resp",
-      "offset": 19
+      "offset": 20
     }
   ],
   "location": "StepDefs.matchEquals(String,String,String,String)"
 });
 formatter.result({
-  "duration": 7857018,
+  "duration": 5587573,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {
-      "val": "SHIPMENTS",
+      "val": "SHIPMENTS2",
       "offset": 4
     },
     {
       "val": "db.readRow(\"SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION C WHERE C.SHIPMENT_ID \u003d\u0027\"+resp+\"\u0027 \")",
-      "offset": 16
+      "offset": 17
     }
   ],
   "location": "StepDefs.def(String,String)"
 });
 formatter.result({
-  "duration": 149487859,
+  "duration": 216255938,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {},
     {
-      "val": "SHIPMENTS.SHIPMENT_ID",
+      "val": "SHIPMENTS2.SHIPMENT_ID",
       "offset": 6
     },
     {},
     {
       "val": "resp",
-      "offset": 31
+      "offset": 32
     }
   ],
   "location": "StepDefs.matchEquals(String,String,String,String)"
 });
 formatter.result({
-  "duration": 10847213,
+  "duration": 6319607,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {
-      "val": "SHIPMENTS",
+      "val": "SHIPMENTS3",
       "offset": 4
     },
     {
       "val": "db.readRows(\"SELECT * FROM INT_BOOK_COMM.COMMITMENT_CONSUMPTION\")",
-      "offset": 16
+      "offset": 17
     }
   ],
   "location": "StepDefs.def(String,String)"
 });
 formatter.result({
-  "duration": 610105447,
+  "duration": 939701037,
   "status": "passed"
 });
 formatter.match({});
